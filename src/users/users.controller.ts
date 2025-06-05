c99a89937cbe1d2d641a67ad5642de7187a34ea0
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-05 17:24:13 +0300

    finist users actions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './users.dto/users.dto';
+import { UpdateUserDto } from './users.dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,6 +20,27 @@ export class UsersController {
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    void this.usersService.create(body.email, body.password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  find(@Query('emial') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
