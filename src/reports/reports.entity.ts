3e00cad54c22afacb2ede6c10b26bf54d2272b77
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-05 16:20:33 +0300

    configure database

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: string;
+}
